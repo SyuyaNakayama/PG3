b889f0225de717d9ef7d98bd7c8a16477818c812
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2022-10-03 17:58:35 +0900

    課題1-2完了

--- main.cpp
+++ main.cpp
@@ -1,30 +1,25 @@
 #include <stdio.h>
 
-template <typename T>
-T Min(T num1, T num2)
+int Wage(unsigned int workTime)
 {
-	if (num1 >= num2) { return num2; }
-	return num1;
-}
-
-template <>
-char Min<char>(char num1, char num2)
-{
-	printf("”šˆÈŠO‚Í“ü—Í‚Å‚«‚Ü‚¹‚ñ\n");
-	return NULL;
+	switch (workTime)
+	{
+	case 0:
+		return 0;
+	case 1:
+		return 100;
+	default:
+		return Wage(workTime - 1) * 2 - 50;
+	}
 }
 
 int main()
 {
-	int iX = 3, iY = 5;
-	float fX = 5.5f, fY = 2.4f;
-	double dX = 10.13, dY = 7.77;
-	char cX = 24, cY = 18;
-
-	printf("%d\n", Min<int>(iX, iY));
-	printf("%f\n", Min<float>(fX, fY));
-	printf("%lf\n", Min<double>(dX, dY));
-	Min<char>(cX, cY);
+	for (size_t i = 1; i <= 10; i++)
+	{
+		printf("ˆê”Ê’À‹à:1072‰~@");
+		printf("Ä‹A’À‹à:%5d‰~\n",Wage(i));
+	}
 
 	return 0;
 }
