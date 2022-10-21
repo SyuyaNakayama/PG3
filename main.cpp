637eb8f29be2724868df3d67bc74da7f64e169b2
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2022-10-21 12:46:35 +0900

    課題1-4完了

--- main.cpp
+++ main.cpp
@@ -2,21 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
-
-int RandamDice()
-{
-	int num = rand() % 6 + 1;
-	printf("出目:%d\n", num);
-	return num;
-}
-
-typedef int (*Dice)();
-
-int Judge(Dice d, int second)
-{
-	Sleep(second * 1000);
-	return d();
-}
+#include <functional>
 
 int main()
 {
@@ -38,11 +24,17 @@ int main()
 		else { printf("正しい数を入力して下さい。\n\n"); }
 	}
 
-	Dice dice = RandamDice;
-
-	printf("判定中です、ちょっと待ってね >o<\n");
+	std::function<int(int)> dice = [](int waitTime)
+	{
+		printf("判定中です、ちょっと待ってね >o<\n");
+		Sleep(waitTime * 1000);
+		int num = rand() % 6 + 1;
+		printf("出目:%d\n", num);
+		return num;
+	};
+	
 	// 判定
-	if (JUDGE_TABLE[select - 1][Judge(dice, 3) % 2]) { printf("大当たり！！\n"); }
+	if (JUDGE_TABLE[select - 1][dice(3) % 2]) { printf("大当たり！！\n"); }
 	else { printf("残念無念再来年...\n"); }
 
 	return 0;
