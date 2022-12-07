25ef3b6a3714f6a46ea7abe501279fc35ddb2f6f
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2022-12-07 12:09:13 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,77 +1,55 @@
 #include <stdio.h>
-#include <list>
-#include <string.h>
 
-using namespace std;
+class SceneManager final
+{
+private:
+	static const char SCENE_NAME[4][10];
+
+	int scene = 0;
+
+	SceneManager() = default;
+	~SceneManager() = default;
+public:
+	static SceneManager* GetInstance();
+	SceneManager(const SceneManager& obj) = delete;
+	SceneManager& operator=(const SceneManager& obj) = delete;
+	void PrintScene() { printf("sceneNo:%d\nsceneName:%s\n\n", scene, SCENE_NAME[scene]); }
+	void ChangeScene(int sceneNo) { scene = sceneNo; }
+};
+
+const char SceneManager::SCENE_NAME[4][10] =
+{
+	"Title","Play","Clear","GameOver"
+};
 
 int main()
 {
-	const char STATION_NAME[28][17] =
+	input:
+	SceneManager* sceneManager = SceneManager::GetInstance();
+	while (true)
 	{
-		"Tokyo",
-		"Kanda",
-		"Akihabara",
-		"Okachimachi",
-		"Ueno",
-		"Uguisudani",
-		"Nippori",
-		"Tabata",
-		"Komagome",
-		"Sugamo",
-		"Otsuka",
-		"Ikebukuro",
-		"Mejiro",
-		"Takadanobaba",
-		"Shin-Okubo",
-		"Shinjuku",
-		"Yoyogi",
-		"Harajuku",
-		"Shibuya",
-		"Ebisu",
-		"Meguro",
-		"Gotanda",
-		"Osaki",
-		"Shinagawa",
-		"Tamachi",
-		"Hamamatsucho",
-		"Shimbashi",
-		"Yurakucho"
-	};
+		sceneManager->PrintScene();
 
-	list<char*> yamanote1970, yamanote2019, yamanote2022;
-	list<char*>::iterator it;
+		int putNum = 0;
+		printf("切り替えたいシーンを選択してください。(0~3, 9で終了)\n");
+		scanf_s("%d", &putNum);
 
-	for (const char* str : STATION_NAME)
-	{
-		yamanote1970.push_back((char*)str);
-	}
-
-	yamanote2019 = yamanote1970;
-	for (it = yamanote2019.begin(); it != yamanote2019.end(); it++)
-	{
-		if (*it == STATION_NAME[7])
+		switch (putNum)
 		{
-			yamanote2019.insert(it, (char*)"Nishi-Nippori");
+		case 0:	case 1:	case 2:	case 3:
+			sceneManager->ChangeScene(putNum);
+			break;
+		case 9:	return 0;
+		default:
+			printf("値が無効です。\n");
+			goto input;
+			break;
 		}
 	}
+}
 
-	yamanote2022 = yamanote2019;
-	for (it = yamanote2022.begin(); it != yamanote2022.end(); it++)
-	{
-		if (*it == STATION_NAME[24])
-		{
-			yamanote2022.insert(it, (char*)"Takanawa-Gateway");
-		}
-	}
-
-	printf("1970年の駅\n");
-	for (char* str : yamanote1970) { printf("%s,", str); }
-
-	printf("\n\n2019年の駅\n");
-	for (char* str : yamanote2019) { printf("%s,", str); }
-
-	printf("\n\n2022年の駅\n");
-	for (char* str : yamanote2022) { printf("%s,", str); }
-
-	return 0;
+SceneManager* SceneManager::GetInstance()
+{
+	static SceneManager sceneManager;
+	return &sceneManager;
 }
